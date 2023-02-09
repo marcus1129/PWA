c8d301474a582292a9b5e625c5fd0f783268cc97
Marcus <Marcus.korre@gmail.com>
2023-02-09 15:04:47 +0100

    Mux Added

--- xillinxProjects/test/test.srcs/sources_1/new/RF.vhd
+++ xillinxProjects/test/test.srcs/sources_1/new/RF.vhd
@@ -24,25 +24,40 @@ component RL
     Port(CLK : in STD_LOGIC;
          Load : in STD_LOGIC_VECTOR(0 to 15);
          DataIn : in STD_LOGIC_VECTOR(0 to 7);
-         DataOutA : out STD_LOGIC_VECTOR(0 to 7);
-         DataOutB : out STD_LOGIC_VECTOR(0 to 7));
+         DataOut0, DataOut1, DataOut2, DataOut3, DataOut4, DataOut5, DataOut6, DataOut7, DataOut8, DataOut9, DataOut10, DataOut11, DataOut12, DataOut13, DataOut14, DataOut15 : out STD_LOGIC_VECTOR(0 to 7));
 end component;
 
 component MUX
-    port(Input : in STD_LOGIC_VECTOR(0 to 7);
+    port(Input0,Input1,Input2,Input3,Input4,Input5,Input6,Input7,Input8,Input9,Input10,Input11,Input12,Input13,Input14,Input15 : in STD_LOGIC_VECTOR(0 to 7);
          SelectAddress : in STD_LOGIC_VECTOR(0 to 3);
          Output : out STD_LOGIC_VECTOR(0 to 7));
 end component;
 
 signal LoadSig: STD_LOGIC_VECTOR(0 to 15);
-signal DataOutASig: STD_LOGIC_VECTOR(0 to 7);
-signal DataOutBSig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut0Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut1Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut2Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut3Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut4Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut5Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut6Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut7Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut8Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut9Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut10Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut11Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut12Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut13Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut14Sig: STD_LOGIC_VECTOR(0 to 7);
+signal DataOut15Sig: STD_LOGIC_VECTOR(0 to 7);
+
 
 begin
 
 DestinationDecoder: DD port map(RW, DA, LoadSig);
-RegisterLogic: RL port map(CLK, LoadSig, D_Data, DataOutASig, DataOutBSig);
-MUXA: MUX port map(DataOutASig, AA, A_Data);
-MUXB: MUX port map(DataOutBSig, BA, B_Data);
+RegisterLogic: RL port map(CLK, LoadSig, D_Data, DataOut0Sig, DataOut1Sig, DataOut2Sig, DataOut3Sig, DataOut4Sig, DataOut5Sig, DataOut6Sig, DataOut7Sig, DataOut8Sig, DataOut9Sig, DataOut10Sig, DataOut11Sig, DataOut12Sig, DataOut13Sig, DataOut14Sig, DataOut15Sig);
+MUXA: MUX port map(DataOut0Sig, DataOut1Sig, DataOut2Sig, DataOut3Sig, DataOut4Sig, DataOut5Sig, DataOut6Sig, DataOut7Sig, DataOut8Sig, DataOut9Sig, DataOut10Sig, DataOut11Sig, DataOut12Sig, DataOut13Sig, DataOut14Sig, DataOut15Sig, AA, A_Data);
+MUXB: MUX port map(DataOut0Sig, DataOut1Sig, DataOut2Sig, DataOut3Sig, DataOut4Sig, DataOut5Sig, DataOut6Sig, DataOut7Sig, DataOut8Sig, DataOut9Sig, DataOut10Sig, DataOut11Sig, DataOut12Sig, DataOut13Sig, DataOut14Sig, DataOut15Sig, BA, B_Data);
+
 
 end Behavioral;
--- xillinxProjects/test/test.srcs/sources_1/new/RL.vhd
+++ xillinxProjects/test/test.srcs/sources_1/new/RL.vhd
@@ -5,8 +5,7 @@ entity RL is
     Port(CLK : in STD_LOGIC;
          Load : in STD_LOGIC_VECTOR(0 to 15);
          DataIn : in STD_LOGIC_VECTOR(0 to 7);
-         DataOutA : out STD_LOGIC_VECTOR(0 to 7);
-         DataOutB : out STD_LOGIC_VECTOR(0 to 7));
+         DataOut0, DataOut1, DataOut2, DataOut3, DataOut4, DataOut5, DataOut6, DataOut7, DataOut8, DataOut9, DataOut10, DataOut11, DataOut12, DataOut13, DataOut14, DataOut15 : out STD_LOGIC_VECTOR(0 to 7));
 end RL;
 
 architecture Behavioral of RL is
@@ -19,21 +18,21 @@ end component;
 
 begin
 
-RC0 : RegisterCell port map(Load(0), DataIn, DataOutA);
-RC1 : RegisterCell port map(Load(1), DataIn, DataOutA);
-RC2 : RegisterCell port map(Load(2), DataIn, DataOutA);
-RC3 : RegisterCell port map(Load(3), DataIn, DataOutA);
-RC4 : RegisterCell port map(Load(4), DataIn, DataOutA);
-RC5 : RegisterCell port map(Load(5), DataIn, DataOutA);
-RC6 : RegisterCell port map(Load(6), DataIn, DataOutA);
-RC7 : RegisterCell port map(Load(7), DataIn, DataOutA);
-RC8 : RegisterCell port map(Load(8), DataIn, DataOutA);
-RC9 : RegisterCell port map(Load(9), DataIn, DataOutA);
-RC10 : RegisterCell port map(Load(10), DataIn, DataOutA);
-RC11 : RegisterCell port map(Load(11), DataIn, DataOutA);
-RC12 : RegisterCell port map(Load(12), DataIn, DataOutA);
-RC13 : RegisterCell port map(Load(13), DataIn, DataOutA);
-RC14 : RegisterCell port map(Load(14), DataIn, DataOutA);
-RC15 : RegisterCell port map(Load(15), DataIn, DataOutA);
+RC0 : RegisterCell port map(Load(0), DataIn, DataOut0);
+RC1 : RegisterCell port map(Load(1), DataIn, DataOut1);
+RC2 : RegisterCell port map(Load(2), DataIn, DataOut2);
+RC3 : RegisterCell port map(Load(3), DataIn, DataOut3);
+RC4 : RegisterCell port map(Load(4), DataIn, DataOut4);
+RC5 : RegisterCell port map(Load(5), DataIn, DataOut5);
+RC6 : RegisterCell port map(Load(6), DataIn, DataOut6);
+RC7 : RegisterCell port map(Load(7), DataIn, DataOut7);
+RC8 : RegisterCell port map(Load(8), DataIn, DataOut8);
+RC9 : RegisterCell port map(Load(9), DataIn, DataOut9);
+RC10 : RegisterCell port map(Load(10), DataIn, DataOut10);
+RC11 : RegisterCell port map(Load(11), DataIn, DataOut11);
+RC12 : RegisterCell port map(Load(12), DataIn, DataOut12);
+RC13 : RegisterCell port map(Load(13), DataIn, DataOut13);
+RC14 : RegisterCell port map(Load(14), DataIn, DataOut14);
+RC15 : RegisterCell port map(Load(15), DataIn, DataOut15);
 
 end Behavioral;
