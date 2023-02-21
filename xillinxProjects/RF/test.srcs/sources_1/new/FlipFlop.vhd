899854ea58308de13f54ec91c44d3d0b3c0ab6eb
Marcus <Marcus.korre@gmail.com>
2023-02-21 07:55:16 +0100

    TB added, flipflop works

--- xillinxProjects/RF/test.srcs/sources_1/new/FlipFlop.vhd
+++ xillinxProjects/RF/test.srcs/sources_1/new/FlipFlop.vhd
@@ -11,24 +11,34 @@ end FlipFlop;
 
 architecture Behavioral of FlipFlop is
 signal OutputSig: STD_LOGIC;
+signal InputSig: STD_LOGIC;
+signal S: STD_LOGIC;
+signal R: STD_LOGIC;
+signal Q: STD_LOGIC;
+signal Qnot: STD_LOGIC;
+
 
 begin
+
+--InputSig <= DataIn and (not Reset);
+--S <= (not InputSig) and (CLK and Enable);
+--R <= InputSig and (CLK and Enable);
+--Q <= S nor Qnot;
+--Qnot <= R nor Q;
+--DataOut <= Q;
+
+
 
 process(CLK, Reset)
-
 begin
-DataOut <= OutputSig;
 if Reset='1' then
-    OutputSig <= '0';
+    DataOut <= '0';
 elsif rising_edge(CLK) then
     if Enable='1' then
-        OutputSig <= DataIn;
+       DataOut <= DataIn;
     else null;
     end if;
 end if;
-
-
-
 end process;
 
 end Behavioral;
